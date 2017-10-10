eb146caa495a692dfe400fc4b253fd58b69945a6
diegovd0296 <diegovd0296@gmail.com>
2017-10-10 16:50:43 +0200

    Greedy

--- Salesman/Greedy.cpp
+++ Salesman/Greedy.cpp
@@ -5,6 +5,39 @@
 
 CTrack CGraph::SalesmanTrackGreedy(CVisits &visits) 
 {
+	
+	CVertex* next = visits.m_Vertices.front();
+	double min_dist;
+	CVertex* aux;
+
 	CTrack track(this);
+	
+	aux = visits.m_Vertices.back();
+	visits.m_Vertices.pop_back();
+	track.AddFirst(visits.m_Vertices.front());
+	visits.m_Vertices.pop_front();
+
+	
+	while (!visits.m_Vertices.empty()) {
+
+		CGraph::Dijkstra(next);
+		min_dist = numeric_limits<double>::max();
+		for (CVertex* c : visits.m_Vertices) {
+			if (min_dist > c->m_DijkstraDistance) {
+				min_dist = c->m_DijkstraDistance;
+				next = c;
+			}
+			
+		}
+		visits.m_Vertices.remove(next);
+		next->m_pDijkstraPrevious = track.m_Vertices.back();
+		track.AddLast(next);
+
+	}
+	track.AddLast(aux);
 	return track;
+
+
+	
+
 }
