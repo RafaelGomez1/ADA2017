1a86de0814264b7a452ffe56bcdfcfa09461ddb6
Diego <diegovd0296@gmail.com>
2017-12-12 12:02:47 +0100

    (done) bb

--- Salesman/BranchAndBound.cpp
+++ Salesman/BranchAndBound.cpp
@@ -1,67 +1,76 @@
 #include "stdafx.h"
 #include "CGraph.h"
 
-class Aux_node {
+
+class bNode {
 public:
-	Aux_node(list<int> to_visit, double h, double cost, list<int> sol);
-	list<int> to_visit;
+	bNode(vector<int> to_visit, double h, double cost, vector<int> solution,double benchmark);
+	vector<int> to_visit;
 	double h;
 	double cost;
-	list<int> sol;
+	vector<int> solution;
+	double benchmark;
+
+
 
 };
-Aux_node::Aux_node(list<int> to_visit, double h, double cost, list<int> sol) {
+bNode::bNode(vector<int> to_visit, double h, double cost, vector<int> solution,double benchmark) {
 	this->to_visit = to_visit;
 	this->cost = cost;
 	this->h = h;
-	this->sol = sol;
+	this->solution = solution;
+	this->benchmark = benchmark;
 }
 
-
-struct CompareDistance {
-	bool operator()(Aux_node const *i1, Aux_node const *i2) {
-		return i1->h < i2->h;
+struct compare_h2_h1 {
+	bool operator()(bNode const *i1, bNode const *i2) {
+		return (i1->h < i2->h);
 	}
 };
 
+struct compare_h3 {
+	bool operator()(bNode const *i1, bNode const *i2) {
+		return (i1->h > i2->h);
+	}
+};
+
+
+
 // SalesmanTrackBranchAndBound1 ===================================================
-/*
-Siempre completar el camino parcial más corto de los que se hayan creado. En el caso
-de haber encontrado un camino completo, se podarán los caminos parciales más largos
-que el mejor camino completo que se haya encontrado hasta el momento.
-*/
+
 CTrack CGraph::SalesmanTrackBranchAndBound1(CVisits &visits)
 {
 	int n_vertex = visits.m_Vertices.size();
-	priority_queue<Aux_node*, vector<Aux_node*>, CompareDistance> pq;
+	priority_queue<bNode*, vector<bNode*>, compare_h2_h1> pq;
 	double best_cost = numeric_limits<double>::max();
-	list<int> visited;
-	list<int> best_solution;
-	list<int> not_visited;
+	vector<int> visited;
+	vector<int> best_solution;
+	vector<int> not_visited;
+
 
 	vector<double> matrix;
 	matrix.resize(n_vertex*n_vertex);
 	vector<CVertex*> vec{ begin(visits.m_Vertices), end(visits.m_Vertices) };
 
 	int destination;
-	int origin;
-	for (origin = 0; origin < n_vertex; origin++) {
-		CGraph::Dijkstra(vec[origin]);
+	for (int i = 0; i < n_vertex; i++) {
+		CGraph::Dijkstra(vec[i]);
 		for (destination = 0; destination < n_vertex; destination++) {
-			matrix[origin*n_vertex + destination] = (vec[destination]->m_DijkstraDistance);
+			matrix[i*n_vertex + destination] = (vec[destination]->m_DijkstraDistance);
 		}
 	}
 
-	visited.push_front(0);
 
-	CVertex *first = visits.m_Vertices.front();
-	CVertex *last = visits.m_Vertices.back();
+	visited.push_back(0);
+
+	CVertex* first = visits.m_Vertices.front();
+	CVertex* last = visits.m_Vertices.back();
 	visits.m_Vertices.pop_back();
 	visits.m_Vertices.pop_front();
 
 	if (n_vertex <= 2) {
-		for (int nv = 0; nv < n_vertex; nv++) {
-			best_solution.push_back(nv);
+		for (int i = 0; i < n_vertex; i++) {
+			best_solution.push_back(i);
 		}
 		goto end1;
 	}
@@ -71,29 +80,30 @@ CTrack CGraph::SalesmanTrackBranchAndBound1(CVisits &visits)
 		not_visited.push_back(count);
 		count++;
 	}
-	Aux_node *current_node = new Aux_node(not_visited, 0, 0, visited);
-	Aux_node *new_node;
-	pq.push(current_node);
-
+	bNode *current = new bNode(not_visited, 0, 0, visited,0);
+	bNode *new_node;
+	pq.push(current);
 	int actual;
+	int val;
 	while (!pq.empty()) {
-		current_node = pq.top();
+		current = pq.top();
 		pq.pop();
-		if (current_node->h < best_cost) {
-			actual = current_node->sol.back();
-			for (int i : current_node->to_visit) {
-				if (current_node->cost + matrix[actual*n_vertex + i] < best_cost) {
-					new_node = new Aux_node(current_node->to_visit, 0, current_node->cost, current_node->sol);
-					new_node->cost += matrix[actual*n_vertex + i];
+		if (current->h < best_cost) {
+			actual = current->solution.back();
+			for (int i = 0; i < current->to_visit.size(); i++) {
+				val = current->to_visit[i];
+				if (current->cost + matrix[actual*n_vertex + val] < best_cost) {
+					new_node = new bNode(current->to_visit, 0, current->cost, current->solution,0);
+					new_node->cost += matrix[actual*n_vertex + val];
 					new_node->h = new_node->cost;
-					new_node->to_visit.remove(i);
-					new_node->sol.push_back(i);
+					new_node->to_visit.erase(new_node->to_visit.begin() + i);
+					new_node->solution.push_back(val);
 
 					if (new_node->to_visit.empty()) {
-						new_node->cost += matrix[(new_node->sol.back()*n_vertex) + n_vertex - 1];
+						new_node->cost += matrix[(new_node->solution.back()*n_vertex) + n_vertex - 1];
 						if (new_node->cost < best_cost) {
-							new_node->sol.push_back(n_vertex - 1);
-							best_solution = new_node->sol;
+							new_node->solution.push_back(n_vertex - 1);
+							best_solution = new_node->solution;
 							best_cost = new_node->cost;
 						}
 						else {
@@ -112,28 +122,28 @@ CTrack CGraph::SalesmanTrackBranchAndBound1(CVisits &visits)
 				}
 			}
 		}
-		delete current_node;
+		delete current;
 	}
 end1:
 	CTrack bestTrack(this);
 	CTrack partialTrack(this);
-	int origin_p = best_solution.front();
-	CVertex *origin_v = vec[origin_p];
-	int destination_p;
-	CVertex *destination_v;
-	best_solution.pop_front();
+	int p_origin = best_solution.front();
+	CVertex *v_origin = vec[p_origin];
+	int p_destination;
+	CVertex *v_destination;
+	best_solution[0];
 	bestTrack.AddFirst(first);
-	for (int sol : best_solution) {
+	for (int i : best_solution) {
 		partialTrack.Clear();
-		destination_p = sol;
-		destination_v = vec[destination_p];
-		Dijkstra(vec[origin_p]);
-		while (origin_v != destination_v) {
-			partialTrack.AddFirst(destination_v);
-			destination_v = destination_v->m_pDijkstraPrevious;
+		p_destination = i;
+		v_destination = vec[p_destination];
+		Dijkstra(vec[p_origin]);
+		while (v_origin != v_destination) {
+			partialTrack.AddFirst(v_destination);
+			v_destination = v_destination->m_pDijkstraPrevious;
 		}
-		origin_p = destination_p;
-		origin_v = vec[origin_p];
+		p_origin = p_destination;
+		v_origin = vec[p_origin];
 		bestTrack.Append(partialTrack);
 
 	}
@@ -145,39 +155,40 @@ end1:
 CTrack CGraph::SalesmanTrackBranchAndBound2(CVisits &visits)
 {
 	double best_cost = numeric_limits<double>::max();
-	double cost = best_cost;
-	priority_queue<Aux_node*, vector<Aux_node*>, CompareDistance> pq;
-	list<int> visited;
-	list<int> solution;
-	list<int> to_visit;
+	double evaluate = best_cost;
+	priority_queue<bNode*, vector<bNode*>, compare_h2_h1> pq;
+	vector<int> visited;
+	vector<int> best_solution;
+	vector<int> to_visit;
 	int n_vertex = visits.m_Vertices.size();
 	vector<double> matrix;
 	matrix.resize(n_vertex*n_vertex);
 	vector<CVertex*> vec{ begin(visits.m_Vertices), end(visits.m_Vertices) };
 
-	int destination;
-	int origin;
-	for (origin = 0; origin < n_vertex; origin++) {
-		CGraph::Dijkstra(vec[origin]);
-		for (destination = 0; destination < n_vertex; destination++) {
-			matrix[origin*n_vertex + destination] = (vec[destination]->m_DijkstraDistance);
+	int j;
+	int i;
+	for ( i = 0; i < n_vertex; i++) {
+		CGraph::Dijkstra(vec[i]);
+		for (j = 0; j < n_vertex; j++) {
+			matrix[i*n_vertex + j] = (vec[j]->m_DijkstraDistance);
 		}
 	}
 
 	vector<double> min_cost;
 	min_cost.resize(n_vertex);
 	double min;
-	for (destination = 0; destination < n_vertex; destination++) {
-		min = matrix[0 + destination];
-		for (origin = 0; origin < n_vertex; origin++) {
-			if (matrix[origin*n_vertex + destination] < min && origin != destination) {
-				min = matrix[origin*n_vertex + destination];
+	for (j = 0; j < n_vertex; j++) {
+		min = matrix[0 + j];
+		for (int i = 0; i < n_vertex; i++) {
+			if (matrix[i*n_vertex + j] < min && i != j) {
+				min = matrix[i*n_vertex + j];
 			}
-			min_cost[destination] = min;
+			min_cost[j] = min;
 		}
 	}
 
-	visited.push_front(0);
+
+	visited.push_back(0);
 
 	CVertex *first = visits.m_Vertices.front();
 	CVertex *last = visits.m_Vertices.back();
@@ -186,13 +197,13 @@ CTrack CGraph::SalesmanTrackBranchAndBound2(CVisits &visits)
 
 	if (n_vertex <= 2) {
 		for (int i = 0; i < n_vertex; i++) {
-			solution.push_back(i);
+			best_solution.push_back(i);
 		}
 		goto end2;
 	}
 
 	int count = 1;
-	for (CVertex *pCount : visits.m_Vertices) {
+	for (CVertex *n : visits.m_Vertices) {
 		to_visit.push_back(count);
 		count++;
 	}
@@ -203,32 +214,32 @@ CTrack CGraph::SalesmanTrackBranchAndBound2(CVisits &visits)
 	}
 	h += min_cost[n_vertex - 1];
 	int actual;
-	int lastVertex = n_vertex - 1;
+	int previous = n_vertex - 1;
 	double h_actual;
-	double new_cost;
-
-	Aux_node *current_node = new Aux_node(to_visit, h, 0, visited);
-	Aux_node *new_node;
-	pq.push(current_node);
+	double updated_cost;
+	bNode *current = new bNode(to_visit, h, 0, visited,0);
+	bNode *new_node;
+	int val;
+	pq.push(current);
 	while (!pq.empty()) {
-		current_node = pq.top();
+		current = pq.top();
 		pq.pop();
-		h_actual = current_node->h;
+		h_actual = current->h;
 		if (h_actual < best_cost) {
-			actual = current_node->sol.back();
-			for (int i : current_node->to_visit) {
-				new_cost = current_node->cost + matrix[actual*n_vertex + i];
-				h = h_actual - min_cost[i] + matrix[actual*n_vertex + i];
+			actual = current->solution.back();
+			for (int i = 0; i < current->to_visit.size(); i++) {
+				val = current->to_visit[i];
+				updated_cost = current->cost + matrix[actual*n_vertex + val];
+				h = h_actual - min_cost[val] + matrix[actual*n_vertex + val];
 				if (h < best_cost) {
-					new_node = new Aux_node(current_node->to_visit, h, new_cost, current_node->sol);
-					new_node->to_visit.remove(i);
-					new_node->sol.push_back(i);
+					new_node = new bNode(current->to_visit, h, updated_cost, current->solution,0);
+					new_node->to_visit.erase(new_node->to_visit.begin() + i);
+					new_node->solution.push_back(val);
 					if (new_node->to_visit.empty()) {
-
-						new_node->cost += matrix[(new_node->sol.back()*n_vertex) + lastVertex];
+						new_node->cost += matrix[(new_node->solution.back()*n_vertex) + previous];
 						if (new_node->cost < best_cost) {
-							new_node->sol.push_back(lastVertex);
-							solution = new_node->sol;
+							new_node->solution.push_back(previous);
+							best_solution = new_node->solution;
 							best_cost = new_node->cost;
 						}
 						else {
@@ -242,21 +253,19 @@ CTrack CGraph::SalesmanTrackBranchAndBound2(CVisits &visits)
 				}
 			}
 		}
-		else {
-			goto end2;
-		}
-		delete current_node;
+
+		delete current;
 	}
 end2:
 	CTrack bestTrack(this);
 	CTrack partialTrack(this);
-	int p_origin = solution.front();
+	int p_origin = best_solution.front();
 	CVertex *v_origin = vec[p_origin];
 	int p_destination;
 	CVertex *v_destination;
-	solution.pop_front();
+	best_solution[0];
 	bestTrack.AddFirst(first);
-	for (int i : solution) {
+	for (int i : best_solution) {
 		partialTrack.Clear();
 		p_destination = i;
 		v_destination = vec[p_destination];
@@ -278,23 +287,21 @@ end2:
 CTrack CGraph::SalesmanTrackBranchAndBound3(CVisits &visits)
 {
 	double best_cost = numeric_limits<double>::max();
-	double cost = best_cost;
-	priority_queue<Aux_node*, vector<Aux_node*>, CompareDistance> pq;
-	list<int> visited;
-	list<int> best_solution;
-	list<int> to_visit;
+	double update_cost = best_cost;
+	priority_queue<bNode*, vector<bNode*>, compare_h3> pq;
+	vector<int> visited;
+	vector<int> best_solution;
+	vector<int> to_visit;
 	int n_vertex = visits.m_Vertices.size();
-
 	vector<double> matrix;
 	matrix.resize(n_vertex*n_vertex);
 	vector<CVertex*> vec{ begin(visits.m_Vertices), end(visits.m_Vertices) };
 
 	int destination;
-	int origin;
-	for (origin = 0; origin < n_vertex; origin++) {
-		CGraph::Dijkstra(vec[origin]);
+	for (int i = 0; i < n_vertex; i++) {
+		CGraph::Dijkstra(vec[i]);
 		for (destination = 0; destination < n_vertex; destination++) {
-			matrix[origin*n_vertex + destination] = (vec[destination]->m_DijkstraDistance);
+			matrix[i*n_vertex + destination] = (vec[destination]->m_DijkstraDistance);
 		}
 	}
 
@@ -303,16 +310,16 @@ CTrack CGraph::SalesmanTrackBranchAndBound3(CVisits &visits)
 	double min;
 	for (destination = 0; destination < n_vertex; destination++) {
 		min = matrix[0 + destination];
-		for (origin = 0; origin < n_vertex; origin++) {
-			if (matrix[origin*n_vertex + destination] < min && origin != destination) {
-				min = matrix[origin*n_vertex + destination];
+		for (int i = 0; i < n_vertex; i++) {
+			if (matrix[i*n_vertex + destination] < min && i != destination) {
+				min = matrix[i*n_vertex + destination];
 			}
 			min_cost[destination] = min;
 		}
 	}
 
 
-	visited.push_front(0);
+	visited.push_back(0);
 
 	CVertex *first = visits.m_Vertices.front();
 	CVertex *last = visits.m_Vertices.back();
@@ -333,44 +340,45 @@ CTrack CGraph::SalesmanTrackBranchAndBound3(CVisits &visits)
 	}
 
 	double h = 0;
-	for (int he : to_visit) {
-		h += min_cost[he];
+	for (int i : to_visit) {
+		h += min_cost[i];
 	}
 	h += min_cost[n_vertex - 1];
 	int actual;
-	int lastVertex = n_vertex - 1;
+	int previous = n_vertex - 1;
+	double lastcost = min_cost[previous];
 	double h_actual;
-	double new_cost;
-
-	Aux_node *current_node = new Aux_node(to_visit, h, 0, visited);
-	Aux_node *new_node;
-	pq.push(current_node);
+	double updated_cost;
+	double best = best_cost;
+	bNode *current = new bNode(to_visit, h, 0, visited, h);
+	bNode *new_node;
+	pq.push(current);
+	int ival;
 	while (!pq.empty()) {
-		current_node = pq.top();
+		current = pq.top();
 		pq.pop();
-		h_actual = current_node->h;
+		h_actual = current->benchmark;
 		if (h_actual < best_cost) {
-			actual = current_node->sol.back();
-			for (int f : current_node->to_visit) {
-
-				new_cost = current_node->cost + matrix[actual*n_vertex + f];
-				h = h_actual - min_cost[f] + matrix[actual*n_vertex + f];
+			actual = current->solution.back();
+			for (int i = 0; i < current->to_visit.size(); i++) {
+				ival = current->to_visit[i];
+				updated_cost = current->cost + matrix[actual*n_vertex + ival];
+				h = h_actual - min_cost[ival] + matrix[actual*n_vertex + ival];
 				if (h < best_cost) {
-					new_node = new Aux_node(current_node->to_visit, h, new_cost, current_node->sol);
-					new_node->to_visit.remove(f);
-					new_node->sol.push_back(f);
+					new_node = new bNode(current->to_visit, h, updated_cost, current->solution, h);
+					new_node->to_visit.erase(new_node->to_visit.begin() + i);
+					new_node->solution.push_back(ival);
+					new_node->h = (new_node->h) * (new_node->to_visit.size());
 					if (new_node->to_visit.empty()) {
-						new_node->cost += matrix[(new_node->sol.back()*n_vertex) + lastVertex];
+						new_node->cost += matrix[(new_node->solution.back()*n_vertex) + previous];
 						if (new_node->cost < best_cost) {
-							new_node->sol.push_back(lastVertex);
-
-							best_solution = new_node->sol;
+							new_node->solution.push_back(previous);
+							best_solution = new_node->solution;
 							best_cost = new_node->cost;
 						}
 						else {
 							delete new_node;
 						}
-
 					}
 					else {
 						pq.push(new_node);
@@ -378,30 +386,28 @@ CTrack CGraph::SalesmanTrackBranchAndBound3(CVisits &visits)
 				}
 			}
 		}
-		else {
-			goto end2;
-		}
-		delete current_node;
+
+		delete current;
 	}
 end2:
 	CTrack bestTrack(this);
 	CTrack partialTrack(this);
 	int p_origin = best_solution.front();
 	CVertex *v_origin = vec[p_origin];
-	int p_desti;
-	CVertex *v_desti;
-	best_solution.pop_front();
+	int p_destination;
+	CVertex *v_destination;
+	best_solution[0];
 	bestTrack.AddFirst(first);
-	for (int i : best_solution) {
+	for (int n : best_solution) {
 		partialTrack.Clear();
-		p_desti = i;
-		v_desti = vec[p_desti];
+		p_destination = n;
+		v_destination = vec[p_destination];
 		Dijkstra(vec[p_origin]);
-		while (v_origin != v_desti) {
-			partialTrack.AddFirst(v_desti);
-			v_desti = v_desti->m_pDijkstraPrevious;
+		while (v_origin != v_destination) {
+			partialTrack.AddFirst(v_destination);
+			v_destination = v_destination->m_pDijkstraPrevious;
 		}
-		p_origin = p_desti;
+		p_origin = p_destination;
 		v_origin = vec[p_origin];
 		bestTrack.Append(partialTrack);
 
